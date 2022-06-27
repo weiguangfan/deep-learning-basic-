cc27a8b1cc51744f4ff695eaf40f5a7280b85e84
weiguangfan <826008728@qq.com>
2022-06-27 17:59:47 +0800

    modify files

--- 2.7 推理处理.py
+++ 2.7 推理处理.py
@@ -1,26 +1,38 @@
-import pickle
+"""
+mnist数据集实现神经网络的推理；
+输入层784个神经元，输出层10个神经元；
 
+"""
+import pickle
 import numpy as np
 from dataset.mnist import load_mnist
+
 
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
 
+
 def soft_max(a):
+    """进化的防止溢出的激活函数"""
     c = np.max(a)
     exp_a = np.exp(a - c)
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
     return y
 
+
 def get_data():
+    """获取数据"""
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
     return x_test, t_test
 
+
 def init_network():
+    """读入保存的pickle文件"""
     with open("sample_weight.pkl", 'rb') as f:
         network = pickle.load(f)
     return network
+
 
 def predict(network, x):
     w1, w2, w3 = network['W1'], network['W2'], network['W3']
@@ -33,22 +45,14 @@ def predict(network, x):
     y = soft_max(a3)
     return y
 
-x, t = get_data()
+
+x, t = get_data()  # t输出真实类别标签的数组
 network = init_network()
 accuracy_cnt = 0
 for i in range(len(x)):
-    y = predict(network, x[i])
-    p = np.argmax(y)
+    y = predict(network, x[i])  # 以数组形式输出各个标签对应的概率
+    p = np.argmax(y)  # 取出概率列表最大值的索引
     if p == t[i]:
         accuracy_cnt += 1
 print("accuracy_cnt: " + str(float(accuracy_cnt / len(x))))
-
-
-
-
-
-
-
-
-
 
