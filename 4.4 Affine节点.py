0d8c177820214af7f5527b029fc2d78d83fc845d
weiguangfan <826008728@qq.com>
2022-06-28 18:38:55 +0800

    modify files

--- 4.4 Affine节点.py
+++ 4.4 Affine节点.py
@@ -1,3 +1,8 @@
+"""
+神经网络的正向传播中，为了计算加权信号和，使用了矩阵的乘积运算（numpy 中是np.dot()）；
+
+"""
+# 单个数据对象的affine层
 import numpy as np
 # x = np.random.rand(2)
 # w = np.random.rand(2,3)
@@ -11,25 +16,28 @@ import numpy as np
 # print(y)
 # print(y.shape)
 
-# x_dot_w = np.array([[0,0,0],[10,10,10]])
-# print(x_dot_w)
-# print(x_dot_w.shape)
-# b = np.array([1,2,3])
-# print(b.shape)
-# print(b)
-#
-# y = x_dot_w + b
-# print(y)
-# print(y.shape)
-#
-# dy = np.array([[1,2,3],[4,5,6]])
-# print(dy.shape)
-# print(dy)
-# db = np.sum(dy,axis=0)
-# print(db.shape)
-# print(db)
+# 批版本的affine层
+# 求导变成了矩阵求导
+x_dot_w = np.array([[0,0,0],[10,10,10]])
+print(x_dot_w)
+print(x_dot_w.shape)
+b = np.array([1,2,3])
+print(b.shape)
+print(b)
+
+y = x_dot_w + b
+print(y)
+print(y.shape)
+# 根据求导公式，
+dy = np.array([[1,2,3],[4,5,6]])
+print(dy.shape)
+print(dy)
+db = np.sum(dy, axis=0)
+print(db.shape)
+print(db)
 
 class Affine(object):
+    """矩阵乘积运算，改写为affine层"""
     def __init__(self, w, b):
         self.w = w
         self.b = b
