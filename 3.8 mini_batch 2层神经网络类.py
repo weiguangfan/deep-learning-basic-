465bb64772e5a3da1739f9b48080199edbcfd893
weiguangfan <826008728@qq.com>
2022-06-28 16:11:06 +0800

    modify files

--- 3.8 mini_batch 2层神经网络类.py
+++ 3.8 mini_batch 2层神经网络类.py
@@ -1,3 +1,7 @@
+"""
+
+"""
+
 import sys, os
 sys.path.append(os.pardir)
 import numpy as np
@@ -5,10 +9,12 @@ from  dataset.mnist import load_mnist
 
 
 def sigmoid(x):
+    """sigmoid激活函数"""
     return 1/(1 + np.exp(-x))
 
 
 def numerical_gradient(f, x):
+    """遍历数组的各个元素，进行数值微分"""
     h = 1e-4
     grad = np.zeros_like(x)
 
@@ -39,6 +45,7 @@ def numerical_gradient(f, x):
 
 
 def soft_max(a):
+    """输出层的softmax激活函数"""
     c = np.max(a)
     exp_a = np.exp(a - c)
     sum_exp_a = np.sum(exp_a)
@@ -47,12 +54,13 @@ def soft_max(a):
 
 
 def cross_entropy_error(y, t):
+    """改进版的交叉熵损失函数"""
     delta = 1e-7
     return -np.sum(t * np.log(y + delta))
 
 
 class TwoLayerNet(object):
-
+    """2层网络的实现"""
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.0):
         self.params = {}
         self.params['w1'] = weight_init_std * np.random.randn(input_size, hidden_size)
@@ -92,40 +100,45 @@ class TwoLayerNet(object):
         return grads
 
 
+# 获取mini-=batch
+# 加载数据
 (x_train, t_train), (x_test, t_test), = load_mnist(normalize=True, one_hot_label=True)
 train_loss_list = []
+
+# 超参数
+# 最大迭代次数
 iters_num = 10000
+# 输入层神经元
 train_size = x_train.shape[0]
+# 批数量
 batch_size = 100
+# 学习率
 learning_rate = 0.1
-
+# 初始化实例对象
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
-for i in range(iters_num):
-    batch_mask = np.random.choice(train_size, batch_size)
-    x_batch = x_train[batch_mask]
-    t_batch = t_train[batch_mask]
 
+# 计算梯度
+# 随机梯度下降
+for i in range(iters_num):
+    # 随机选择样本，返回样本索引的数组
+    batch_mask = np.random.choice(train_size, batch_size)
+    # 输入批数据
+    x_batch = x_train[batch_mask]
+    # 标签批数据
+    t_batch = t_train[batch_mask]
+    # 计算各个参数相对于损失函数的梯度
     grad = network.numerical_gradient(x_batch, t_batch)
+
+    # 更新参数
+    # 更新权重和偏置参数，并保存到字典network.params
     for key in ('w1', 'b1', 'w2', 'b2'):
         network.params[key] -= learning_rate * grad[key]
+
+    # 记录学习过程
+    # 计算批数据的总损失
     loss = network.loss(x_batch, t_batch)
+    # 每批数据的损失，均添加到列表
     train_loss_list.append(loss)
 
 print("train_loss_list: ", train_loss_list)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
