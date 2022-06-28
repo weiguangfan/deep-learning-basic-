ca7a7687689ef47860b0f46639f555c3a5d09660
weiguangfan <826008728@qq.com>
2022-06-28 15:10:45 +0800

    modify files

--- 3.6 神经网络梯度.py
+++ 3.6 神经网络梯度.py
@@ -1,14 +1,22 @@
+"""
+神经网络的学习也要求梯度；
+梯度是指损失函数关于权重参数的梯度；
+梯度数组的元素由各个元素关于W的偏导数构成；
+"""
+
 import sys, os
 import numpy as np
 sys.path.append(os.pardir)
 
 
 def cross_entropy_error(y, t):
+    """单个数据的交叉熵损失函数"""
     delta = 1e-7
     return -np.sum(t * np.log(y + delta))
 
 
 def soft_max(a):
+    """改进版的输出层softmax激活函数"""
     c = np.max(a)
     exp_a = np.exp(a - c)
     sum_exp_a = np.sum(exp_a)
@@ -17,6 +25,7 @@ def soft_max(a):
 
 
 def numerical_gradient(f, x):
+    """遍历数组中各个元素，进行数值微分"""
     print("x: ", x)
     h = 1e-4
     grad = np.zeros_like(x)
@@ -55,15 +64,19 @@ def numerical_gradient(f, x):
 
 
 class simpleNet(object):
+    """定义一个简单的神经网络，求梯度"""
     def __init__(self):
+        """定义一个权重参数self.w"""
         self.w = np.random.randn(2, 3)
 
     def predict(self, x):
+        """进行预测"""
         print("self.w: ", self.w)
         print("predict input x: ", x)
         return np.dot(x, self.w)
 
     def loss(self, x, t):
+        """求损失"""
         print("loss_input x: ", x)
         print("loss_input t: ", t)
         z = self.predict(x)
@@ -76,36 +89,41 @@ class simpleNet(object):
         print("loss: ", loss)
         return loss
 
-
+# 实例化对象
 net = simpleNet()
 print("net.w: ", net.w)
 print("net.w.shape: ", net.w.shape)
 
+# 输入数据
 x = np.array([0.6, 0.9])
 print("x: ", x)
 print("x.shape: ", x.shape)
 
-# p = net.predict(x)
-# print(p)
-# print(p.shape)
-# print(np.argmax(p))
+# 输出值
+p = net.predict(x)
+print(p)
+print(p.shape)
+# 返回数组各元素最大值的下标
+print(np.argmax(p))
+
+# 标签数据
 t = np.array([0, 0, 1])
 print("t: ", t)
 print("t.shape: ", t.shape)
-# net.loss(x, t)
+
+# 损失值
+print(net.loss(x, t))
 
 
-# def f(W):
-#     return net.loss(x, t)
+# 求梯度
+# f = lambda w:net.loss(x, t)
+def f(W):
+    """参数w是一个伪参数"""
+    return net.loss(x, t)
 
-f = lambda w:net.loss(x, t)
 
+# 求含有参数w的损失函数的偏导
+# net.w传入函数f
 dw = numerical_gradient(f, net.w)
 print(dw)
-
-
-
-
-
-
 
